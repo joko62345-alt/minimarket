4d409e70c768f37eea0b11bdb39be1bf5deda582
joko62345-alt <joko62345@gmail.com>
2026-09-11 13:04:56 +0700

    tugas

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+</head>
+<body>
+    <h1>Profil Pengembang</h1>
+    <p>Nama: Joko Sutrisno</p>
+    <p>Program Studi: Teknik Informatika</p>
+    <p>Politeknik Negeri Jember</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckMemberStatus;
 
 Route::get('/', function () {
-    return 'Laravel berhasil dijalankan!';
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/profile/{name?}', function ($name = 'Tamu') {
+    return "Nama: " . $name;
+});
+
+Route::get('/product/{id}', function ($id) {
+    return "ID Produk: " . $id;
+})->whereNumber('id');
+
+Route::get('/admin/dashboard', function () {
+    return "Halaman Dashboard Admin";
+})->name('dashboard');
+
+Route::prefix('member')->middleware(CheckMemberStatus::class)->
+group(function () {
+
+    Route::get('/profile', function () {
+        return "Profil Member";
+    });
+
+    Route::get('/settings', function () {
+        return "Pengaturan Member";
+    });
+
 });
